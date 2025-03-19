d8e8bdd483830fc2fcee563c22b1169fbc8afbe9
slidebrass <slidebrass@gmail.com>
2025-03-19 12:23:49 -0500

    dependencies installed

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from 'react-router';
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+    
+    </BrowserRouter>
+  )
+}
+
+export default App
