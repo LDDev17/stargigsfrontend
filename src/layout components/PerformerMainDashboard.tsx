d6c0f1f2385ce2f100e2066ebec67678508c4217
slidebrass <slidebrass@gmail.com>
2025-05-16 11:38:38 -0500

    setting up email authentication

--- src/layout components/PerformerMainDashboard.tsx
+++ src/layout components/PerformerMainDashboard.tsx
@@ -44,76 +44,80 @@ const PerformerMainDashboard = () => {
   return (
     <>
     
-      <div className={`bg-white rounded-xl mt-24 mb-4 flex flex-grow p-8 font-inter 
+      <div className={`bg-white rounded-xl mt-24 mb-4 flex flex-col space-y-4 p-8 font-inter 
           ${isExpanded ? 'ml-60' : 'ml-32'}
         `}
       >
+        {/* Main or everything but Footer */}
+        <main className="flex">
+          {/* Left Side */}
+          <section className="flex flex-col space-y-4 grow pr-4">
+            <header>
+              {/* row 1 */}
+              <div className="flex justify-start items-center space-x-2 pb-4">
+                <h4 className="font-bold text-2xl">Morning Andrew</h4>
+                <p>weather icon</p>
+                <p className="font-lato text-sm">-Here's a quick look at your upcoming gigs, bookings, and new opportunities!</p>
+              </div>
+              {/* row 2 */}
+              <div className="flex justify-between space-x-6">
+                <div className={`bg-[#ffece0] rounded-xl grow`}>
+                  <DashboardTile
+                    title={tileData[0].title}
+                    amount={tileData[0].amount}
+                    percentage={tileData[0].percentage}
+                  />
+                </div>
+                <div className={`bg-[#e6f1fd] rounded-xl grow`}>
+                  <DashboardTile
+                    title={tileData[1].title}
+                    amount={tileData[1].amount}
+                    percentage={tileData[1].percentage}
+                  />
+                </div>
+                <div className={`bg-[#edeefc] rounded-xl grow`}>
+                  <DashboardTile
+                    title={tileData[2].title}
+                    amount={tileData[2].amount}
+                    percentage={tileData[2].percentage}
+                  />
+                </div>
+                <div className={`bg-[#fff9e7] rounded-xl grow`}>
+                  <DashboardTile
+                    title={tileData[3].title}
+                    amount={tileData[3].amount}
+                    percentage={tileData[3].percentage}
+                  />
+                </div>
+              </div>
+            </header>
 
-        {/* Left Side */}
-        <section className="flex flex-col space-y-4 grow pr-4">
-          <header>
-            {/* row 1 */}
-            <div className="flex justify-start items-center space-x-2 pb-4">
-              <h4 className="font-bold text-2xl">Morning Andrew</h4>
-              <p>weather icon</p>
-              <p>-Here's a quick look at your upcoming gigs, bookings, and new opportunities!</p>
-            </div>
-            {/* row 2 */}
-            <div className="flex justify-between space-x-6">
-              <div className={`bg-[#ffece0] rounded-xl grow`}>
-                <DashboardTile
-                  title={tileData[0].title}
-                  amount={tileData[0].amount}
-                  percentage={tileData[0].percentage}
-                />
-              </div>
-              <div className={`bg-[#e6f1fd] rounded-xl grow`}>
-                <DashboardTile
-                  title={tileData[1].title}
-                  amount={tileData[1].amount}
-                  percentage={tileData[1].percentage}
-                />
-              </div>
-              <div className={`bg-[#edeefc] rounded-xl grow`}>
-                <DashboardTile
-                  title={tileData[2].title}
-                  amount={tileData[2].amount}
-                  percentage={tileData[2].percentage}
-                />
-              </div>
-              <div className={`bg-[#fff9e7] rounded-xl grow`}>
-                <DashboardTile
-                  title={tileData[3].title}
-                  amount={tileData[3].amount}
-                  percentage={tileData[3].percentage}
-                />
-              </div>
-            </div>
-          </header>
+            {/* Upcoming Gigs Table */}
+            <section className="bg-[#fbfbfb] p-6 rounded-md">
+              <PerformerDashUpcomingTable />
+            </section>
 
-          {/* Upcoming Gigs Table */}
-          <section className="bg-[#fbfbfb] p-6 rounded-md">
-            <PerformerDashUpcomingTable />
+            
           </section>
 
-          {/* Booking Table */}
-          <footer className="flex justify-between gap-8">
+          {/* Right Side */}
+          <section className="pl-4 flex flex-col justify-start space-y-8">
             <div>
-              <DashBookingTable />
+              <p>Notifications</p>
+              <NotificationSidebar />
             </div>
-            <div>
-              <DashCalendar calendarDate={today} />
-            </div>
-          </footer>
-        </section>
+          </section>
+        </main>
 
-        {/* Right Side */}
-        <section className="pl-4 flex flex-col justify-start space-y-8">
-          <div>
-            <p>Notifications</p>
-            <NotificationSidebar />
+        {/* Booking Table */}
+        <footer className="flex gap-8 h-84">
+          <div className="h-full grow-2">
+            <DashBookingTable />
           </div>
-        </section>
+          <div className="h-full grow-1">
+            <DashCalendar calendarDate={today} />
+          </div>
+        </footer>
       </div>
     
       
